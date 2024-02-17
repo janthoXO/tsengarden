72d3604c452cf1747e997dcee8931b014df629dd
dennis <dennis.jandow@tum.de>
2024-02-18 00:05:11 +0100

    add scrollable button in status view

--- lib/status/statusView.dart
+++ lib/status/statusView.dart
@@ -17,83 +17,91 @@ class StatusView extends StatefulWidget {
 }
 
 class _StatusViewState extends State<StatusView> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           actions: [
-            IconButton(onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => EditPlantView(
-                    editPlantController: EditPlantController(data: widget.statusController.data)
-                ))),
+            IconButton(
+                onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => EditPlantView(
+                            editPlantController: EditPlantController(
+                                data: widget.statusController.data)))),
                 icon: const Icon(Icons.add)),
           ],
         ),
-        body: Column(
-          children: [
-            // WaterButton
-            Expanded(
+        body: NestedScrollView(
+          floatHeaderSlivers: true,
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverToBoxAdapter(
                 child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                  IconButton.filled(
-                      onPressed: () {},
-                      iconSize: 100,
-                      padding: const EdgeInsets.all(10),
-                      icon: const Icon(Icons.water_drop),
-                      style: IconButton.styleFrom(
-                          backgroundColor: Colors.lightBlue)),
-                  const Padding(padding: EdgeInsets.symmetric(vertical: 10)),
-                  Text(
-                    "Driest Plant",
-                    style: Theme.of(context).textTheme.headlineSmall,
-                  ),
-                  Text(
-                    "Overdue xxx days",
-                    style: TextStyle(
-                        color: Theme.of(context).colorScheme.secondary),
-                  ),
-                ])),
+                      IconButton.filled(
+                          onPressed: () {},
+                          iconSize: 100,
+                          padding: const EdgeInsets.all(10),
+                          icon: const Icon(Icons.water_drop),
+                          style: IconButton.styleFrom(
+                              backgroundColor: Colors.lightBlue)),
+                      const Padding(padding: EdgeInsets.symmetric(vertical: 10)),
+                      Text(
+                        "Driest Plant",
+                        style: Theme.of(context).textTheme.headlineSmall,
+                      ),
+                      Text(
+                        "Overdue xxx days",
+                        style: TextStyle(
+                            color: Theme.of(context).colorScheme.secondary),
+                      ),
+                    ]),
+              )
+            ];
+          },
 
-            //WaterList
-            Expanded(
-              child: ListView.separated(
-                padding: const EdgeInsets.all(10),
-                itemCount: widget.statusController.data.ownedPlants.length,
-                itemBuilder: (BuildContext ctx, int index) {
-                  return ElevatedButton(
-                    onPressed: () => Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => DetailPlantView(
+          //WaterList
+          body: ListView.separated(
+            padding: const EdgeInsets.all(10),
+            itemCount: widget.statusController.data.ownedPlants.length,
+            itemBuilder: (BuildContext ctx, int index) {
+              return ElevatedButton(
+                onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => DetailPlantView(
                               ownedPlant: widget
                                   .statusController.data.ownedPlants[index],
                               data: widget.statusController.data,
                             ))),
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: const Size(0, 75),
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                      backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
-                      padding: const EdgeInsets.all(10),
-                    ),
-                    child: Row(children: [
-                      Text(widget.statusController.data.ownedPlants[index].name),
-                      const Spacer(),
-                      Text(
-                        widget.statusController.data.ownedPlants[index].lastWatered.toString().split(" ")[0],
-                        style: TextStyle(color: Theme.of(context).colorScheme.secondary),
-                      ),
-                      const Padding(padding: EdgeInsets.all(5)),
-                      const Icon(Icons.water_drop),
-                    ]),
-                  );
-                },
-                separatorBuilder: (ctx, index) => const Divider(height: 10,color: Color.fromARGB(0,0,0,0)),
-              ),
-            ),
-          ],
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(0, 75),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  backgroundColor:
+                      Theme.of(context).colorScheme.secondaryContainer,
+                  padding: const EdgeInsets.all(10),
+                ),
+                child: Row(children: [
+                  Text(widget.statusController.data.ownedPlants[index].name),
+                  const Spacer(),
+                  Text(
+                    widget.statusController.data.ownedPlants[index].lastWatered
+                        .toString()
+                        .split(" ")[0],
+                    style: TextStyle(
+                        color: Theme.of(context).colorScheme.secondary),
+                  ),
+                  const Padding(padding: EdgeInsets.all(5)),
+                  const Icon(Icons.water_drop),
+                ]),
+              );
+            },
+            separatorBuilder: (ctx, index) =>
+                const Divider(height: 10, color: Color.fromARGB(0, 0, 0, 0)),
+          ),
         ));
   }
 }
